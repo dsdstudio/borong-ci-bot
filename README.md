0a7fff6722cd8f5a51d8b982ab999dfacb252b75
보롱킴 <dsdgun@gmail.com>
2016-01-16 00:04:05 +0900

    기능 대략 구현 완료 'ㅂ'

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ $ npm install -g selenium-standalone
 $ selenium-standalone install --drivers.chrome.version=2.20 --drivers.chrome.baseURL=https://chromedriver.storage.googleapis.com
 $ selenium-standalone start
 ```
+
+### 실행방법 
+
+```
+b_login_id='yourid' b_passwd='yourpassword' nightwatch
+```
--- tests/t0.js
+++ tests/t0.js
@@ -1,13 +1,16 @@
 module.exports = {
 	'도서대출연장': function(b) {
-		b.url('http://gwangjinlib.seoul.kr')
-		.frame('SLIMAplus', function(e) {
-			console.log(e);
+		b.url('http://www.gwangjinlib.seoul.kr/KJL/MyPage/Lend/LnReport.csp?HLOC=KJL&COUNT=38pxeo9q00&Kor=1')
+		.setValue('input[name=UsrID]', process.env.b_login_id)
+		.setValue('input[name=PWord]', process.env.b_passwd)
+		.click('#btn_login')
+		.click('form[name=LnReport] span')
+		.windowHandles(function(w) {
+			var handle = w.value;
+			this.switchWindow(handle[1])
+			this.resizeWindow(500,500)
+			this.click('a');
 		})
-		.element('id', 'UsrID', function(e) {
-			console.log('UsrID', e.value);
-		})
-		.frame(null)
-		
+		.end()
 	}
 };
